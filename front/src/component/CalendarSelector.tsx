93ca331e5e616690175f849fe46fbfa6ac0bc490
euno <chonist@gmail.com>
2024-07-04 20:58:35 +0900

    css update

--- front/src/component/CalendarSelector.tsx
+++ front/src/component/CalendarSelector.tsx
@@ -185,6 +185,8 @@ const DateValue = styled.div`
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  min-height: 350px;
+
   overflow: hidden;
   display: flex;
   flex-direction: column;
