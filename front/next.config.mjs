91f037770828a533df2234e96665100426fbb2c2
euno <chonist@gmail.com>
2024-07-10 10:55:12 +0900

    update next config

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -2,10 +2,13 @@
 
 import nextPWA from 'next-pwa';
 
+const dev = process.env.NODE_ENV === 'development'
+
 const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: dev ? true : false,
 });
 
 const nextConfig = {
