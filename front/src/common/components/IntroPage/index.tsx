3c34b48e4dab9ccba48bda2e104805fcb3d243ae
euno <chonist@gmail.com>
2025-01-02 06:41:13 +0900

    chore: IntroPage 컴포넌트 폴더 구조 변경

---
+++ front/src/common/components/IntroPage/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import IsMobile from "@/common/function/IsMobile";
+import IntroDesktop from "./IntroDesktop";
+import IntroMobile from "./IntroMobile";
+
+const IntroPage = () => {
+  const isMobile = IsMobile();
+
+
+  if (isMobile == null) return <></>;
+  else if (isMobile === true) return <IntroMobile />;
+  else return <IntroDesktop />;
+}
+
+export default IntroPage;
