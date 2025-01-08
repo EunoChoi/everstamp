f665ee8c687325bd4ae5b99bc6ad7e4d6c22a205
euno <chonist@gmail.com>
2025-01-08 10:56:50 +0900

    sytle: calendarView 컴포넌트 태블릿 ui 최적화

--- front/src/common/components/ui/Calendar/index.tsx
+++ front/src/common/components/ui/Calendar/index.tsx
@@ -54,9 +54,13 @@ export default Calendar;
 
 const Wrapper = styled.div`
   width: 100%;
-  /* height: 100%; */
+  height: 100%;
 
   display: flex;
   flex-direction: column;
   flex-grow: 1;
+
+  @media (min-width:480px) and (max-width:1023px) { //mobild land + tablet
+    min-height: 350px;
+  }
 `
