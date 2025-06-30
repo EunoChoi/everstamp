43dce09e24c0ab17cd25b70074704375d6ceeeba
euno <chonist@gmail.com>
2025-06-30 12:52:40 +0900

    fix: nProgressStyles 파일 제거 및 코드 이동

--- front/src/app/(routes)/app/(withLayout)/layout.tsx
+++ front/src/app/(routes)/app/(withLayout)/layout.tsx
@@ -14,9 +14,8 @@ import MobileLayout from '@/common/components/layout/MobileLayout';
 import { ScrollProvider } from "@/common/components/util/ScrollProvider";
 import ServiceWorkerRegister from "@/common/components/util/ServiceWorkerRegister";
 import useCustomRouter from "@/common/hooks/useCustomRouter";
-import NProgressStyles from "@/common/styles/nProgressStyles";
 import { useSnackbar } from 'notistack';
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 interface Props {
   children: ReactNode;
@@ -95,3 +94,58 @@ const AppLayout = ({ children, modal }: Props) => {
 
 export default AppLayout;
 
+const NProgressStyles = createGlobalStyle<{ barColor: string }>`
+  #nprogress {
+    pointer-events: none;
+  }
+
+  #nprogress .bar {
+    background: ${props => props.barColor};
+    position: fixed;
+    z-index: 9999;
+    top: 0;
+    left: 0;
+    width: 100dvw;
+   
+    @media (max-width: 479px) { //mobile port
+      top: auto;
+      bottom: 0;
+      height: 6px;
+    }
+    @media (min-width:480px) and (max-width:1024px) { //mobild land + tablet
+      height: 4px;
+    }
+    @media (min-width:1025px) { //desktop
+      height: 8px;
+    }
+  }
+
+  #nprogress .spinner {
+    display: block;
+    position: fixed;
+    z-index: 1031;
+    top: 40%;
+    right: 50%;
+    transform: translate(50%, 0);
+  }
+
+  #nprogress .spinner-icon {
+    width: 64px;
+    height: 64px;
+    box-sizing: border-box;
+    border: solid 6px transparent;
+    border-top-color: ${props => props.barColor};
+    border-left-color: ${props => props.barColor};
+    border-radius: 50%;
+    animation: nprogress-spinner 350ms ease-in-out infinite;
+  }
+
+  @keyframes nprogress-spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
