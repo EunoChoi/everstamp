fda4794e7357759ce444508bda0a0c26a26b454d
euno <chonist@gmail.com>
2025-07-02 09:18:03 +0900

    fix: 로그아웃, 계정 삭제, SnackBarAction 컴포넌트 적용

--- front/src/app/(routes)/app/(withLayout)/setting/_functions/onDeleteAccount.tsx
+++ front/src/app/(routes)/app/(withLayout)/setting/_functions/onDeleteAccount.tsx
@@ -1,20 +1,19 @@
 import Axios from "@/Axios/axios";
-import $Common from "@/common/styles/common";
+import { SnackBarAction } from "@/common/components/util/snackBar/SnackBarAction";
 import { signOut } from "next-auth/react";
 import { SnackbarKey, closeSnackbar, enqueueSnackbar } from "notistack";
 
 export const onDeleteAccount = () => {
   const userDeleteAction = (snackbarId: SnackbarKey) => (
     <>
-      <$Common.YesOrNo className="no" onClick={() => { closeSnackbar('userDelete'); }}>
-        No
-      </$Common.YesOrNo>
-      < $Common.YesOrNo className="yes" onClick={() => {
-        Axios.delete('user').then(() => { signOut(); });
-        closeSnackbar('userDelete');
-      }}>
-        Yes
-      </ $Common.YesOrNo>
+      <SnackBarAction
+        yesAction={() => {
+          Axios.delete('user').then(() => { signOut(); });
+          closeSnackbar('userDelete');
+        }}
+        noAction={() => {
+          closeSnackbar('userDelete');
+        }} />
     </>
   );
   enqueueSnackbar('회원탈퇴 하시겠습니까?', { key: 'userDelete', persist: true, action: userDeleteAction, autoHideDuration: 6000 });
--- front/src/app/(routes)/app/(withLayout)/setting/_functions/onLogout.tsx
+++ front/src/app/(routes)/app/(withLayout)/setting/_functions/onLogout.tsx
@@ -1,20 +1,19 @@
 import Axios from "@/Axios/axios";
-import $Common from "@/common/styles/common";
+import { SnackBarAction } from "@/common/components/util/snackBar/SnackBarAction";
 import { signOut } from "next-auth/react";
 import { SnackbarKey, closeSnackbar, enqueueSnackbar } from "notistack";
 
 export const onLogout = () => {
   const logoutAction = (snackbarId: SnackbarKey) => (
     <>
-      <$Common.YesOrNo className="no" onClick={() => { closeSnackbar('logout'); }}>
-        No
-      </$Common.YesOrNo>
-      <$Common.YesOrNo className="yes" onClick={() => {
-        Axios.get('user/logout').then(() => { signOut(); });
-        closeSnackbar('logout');
-      }}>
-        Yes
-      </$Common.YesOrNo>
+      <SnackBarAction
+        yesAction={() => {
+          Axios.get('user/logout').then(() => { signOut(); });
+          closeSnackbar('logout');
+        }}
+        noAction={() => {
+          closeSnackbar('logout');
+        }} />
     </>
   );
   enqueueSnackbar('로그아웃 하시겠습니까?', { key: 'logout', persist: true, action: logoutAction, autoHideDuration: 6000 });
