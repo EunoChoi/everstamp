28d54e4a2e7ca39f740f1ccda638d2bfdbf3b5e4
euno <chonist@gmail.com>
2025-07-02 21:43:52 +0900

    chore: 주석 정리

--- front/src/common/components/views/HabitOrderView/HabitList.tsx
+++ front/src/common/components/views/HabitOrderView/HabitList.tsx
@@ -18,22 +18,18 @@ interface HabitListProps {
 //DndContext가 리렌더링되는 경우 초기사태로 돌아가니 드래그 과정 중 리렌더링 되지 않도로 주의해야한다. 
 export const HabitList = memo(({ tempHabits, setTempHabits }: HabitListProps) => {
 
-  // 원리: 마우스, 터치 입력을 모두 감지하는 센서를 만든다.
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(TouchSensor)
   );
 
-  // 원리: 드래그가 끝났을 때 호출될 함수.
   const handleDragEnd = (event: any) => {
     const { active, over } = event;
 
     if (over && active.id !== over.id) {
-      // 상태 업데이트: 부모로부터 받은 setHabits를 사용한다.
       setTempHabits((items) => {
         const oldIndex = items.findIndex((item) => item.id === active.id);
         const newIndex = items.findIndex((item) => item.id === over.id);
-        // arrayMove: dnd-kit이 제공하는 배열 순서 변경 유틸 함수.
         return arrayMove(items, oldIndex, newIndex);
       });
     }
