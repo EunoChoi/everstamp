86d2d068abafc46e9dc2a561d5463a1de014f558
euno <chonist@gmail.com>
2025-07-06 02:06:54 +0900

    fix: 모달, 노티 스택 중첩 가려짐 문제 수정

--- front/src/common/components/util/snackBar/CustomSnackbarProvider.tsx
+++ front/src/common/components/util/snackBar/CustomSnackbarProvider.tsx
@@ -2,51 +2,61 @@
 
 import { MaterialDesignContent, SnackbarProvider } from "notistack";
 import { ReactNode } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components"; // createGlobalStyle 추가
 
 interface Props {
-  children: ReactNode;
+  children?: ReactNode;
 }
 
 const CustomSnackbarProvider = ({ children }: Props) => {
-  return (<SnackbarProvider
-    Components={{
-      default: StyledMaterialDesignContent,
-      info: StyledMaterialDesignContent,
-      success: StyledMaterialDesignContent,
-      error: StyledMaterialDesignContent,
-    }}
-    anchorOrigin={{
-      // vertical: `${isMobile ? 'bottom' : 'top'}`,
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    maxSnack={1}
-    autoHideDuration={2000}
-    preventDuplicate={true}
-  >
-    {children}
-  </SnackbarProvider>);
+  return (
+    <>
+      <GlobalSnackbarStyle />
+      <SnackbarProvider
+        Components={{
+          default: StyledMaterialDesignContent,
+          info: StyledMaterialDesignContent,
+          success: StyledMaterialDesignContent,
+          error: StyledMaterialDesignContent,
+        }}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        maxSnack={1}
+        autoHideDuration={2000}
+        preventDuplicate={true}
+      >
+        {children}
+      </SnackbarProvider>
+    </>
+  );
 }
 
 export default CustomSnackbarProvider;
+
+const GlobalSnackbarStyle = createGlobalStyle`
+  .notistack-SnackbarContainer {
+    z-index: 999999 !important; /* 모달보다 확실히 높은 값 */
+  }
+`;
 
 const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
   '&.notistack-MuiContent-default': {
     backgroundColor: 'whitesmoke',
     color: 'rgb(88, 88, 88)',
-    fontWeight: '500'
+    fontWeight: '500',
   },
   '&.notistack-MuiContent-info': {
     backgroundColor: '#8EBCDB',
-    fontWeight: '500'
+    fontWeight: '500',
   },
   '&.notistack-MuiContent-success': {
     backgroundColor: '#83c6b6',
-    fontWeight: '500'
+    fontWeight: '500',
   },
   '&.notistack-MuiContent-error': {
     backgroundColor: '#dc7889',
-    fontWeight: '500'
+    fontWeight: '500',
   },
 }));
