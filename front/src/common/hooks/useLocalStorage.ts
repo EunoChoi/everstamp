cd2fb137097350b8a9e824450b184714dda1ee20
euno <chonist@gmail.com>
2025-07-07 13:55:00 +0900

    feat: useLocalStorage 훅 추가

---
+++ front/src/common/hooks/useLocalStorage.ts
@@ -0,0 +1,37 @@
+// src/hooks/useLocalStorage.js
+
+import { useState } from 'react';
+
+export function useLocalStorage<T,>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  //local에 값 업데이트 및 저장
+  const setValue = (value: T) => {
+    try {
+      //value가 함수인 경우 처리
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+      //로컬 저장
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { storedValue, setValue };
+}
