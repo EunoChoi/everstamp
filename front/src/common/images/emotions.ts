7cfbd5c64522e80e61306665353afa02b7ae10b1
euno <chonist@gmail.com>
2025-07-14 13:50:49 +0900

    feat: emotion 이미지 불러오기 위한 객체 생성

---
+++ front/src/common/images/emotions.ts
@@ -0,0 +1,13 @@
+import emotion0 from '/public/img/emotion/emotion0.png';
+import emotion1 from '/public/img/emotion/emotion1.png';
+import emotion2 from '/public/img/emotion/emotion2.png';
+import emotion3 from '/public/img/emotion/emotion3.png';
+import emotion4 from '/public/img/emotion/emotion4.png';
+
+export const emotions = [
+  { id: 4, src: emotion4, alt: 'Joyful' },
+  { id: 3, src: emotion3, alt: 'happy' },
+  { id: 2, src: emotion2, alt: 'common' },
+  { id: 1, src: emotion1, alt: 'sad' },
+  { id: 0, src: emotion0, alt: 'angry' },
+];
