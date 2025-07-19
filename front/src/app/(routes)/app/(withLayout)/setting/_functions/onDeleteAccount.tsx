461d2061747228b0af4703708cf6d0e90ffb9b13
euno <chonist@gmail.com>
2025-07-19 09:57:28 +0900

    fix: 회원 탈퇴 처리 방식 변경 : back api -> next server-action

--- front/src/app/(routes)/app/(withLayout)/setting/_functions/onDeleteAccount.tsx
+++ front/src/app/(routes)/app/(withLayout)/setting/_functions/onDeleteAccount.tsx
@@ -1,15 +1,23 @@
-import Api from "@/common/api/Api";
 import { SnackBarAction } from "@/common/utils/snackBar/SnackBarAction";
+import { deleteUser } from "@/server/actions/user.actions";
 import { signOut } from "next-auth/react";
 import { SnackbarKey, closeSnackbar, enqueueSnackbar } from "notistack";
+import { startTransition } from "react";
 
 export const onDeleteAccount = () => {
   const userDeleteAction = (snackbarId: SnackbarKey) => (
     <>
       <SnackBarAction
         yesAction={() => {
-          Api.delete('user').then(() => { signOut(); });
-          closeSnackbar('userDelete');
+          startTransition(async () => {
+            const result = await deleteUser();
+            if (result.success) {
+              enqueueSnackbar(result.message, { variant: 'success' });
+              signOut({ callbackUrl: '/' });
+            } else {
+              enqueueSnackbar(result.message, { variant: 'error' });
+            }
+          });
         }}
         noAction={() => {
           closeSnackbar('userDelete');
