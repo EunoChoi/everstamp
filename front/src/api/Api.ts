6747ce7c547ce1615a77c39c42348e9710bf5576
euno <chonist@gmail.com>
2026-01-23 18:49:18 +0900

    fix: fix baseurl

--- front/src/api/Api.ts
+++ front/src/api/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = `${process.env.NEXT_PUBLIC_DOMAIN}/api`;
+const baseURL = `http://${process.env.NEXT_PUBLIC_DOMAIN}`;
 
 const api = axios.create({
   baseURL,
